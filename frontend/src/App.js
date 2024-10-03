5ed5875f248b0b95911704ed337991afe33d06e3
Shagun Sharma <shagun.sharma@slu.edu>
2024-10-03 01:49:24 -0500

    Add auth pages routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
-import ScrollToTop from "./utils/ScrollToTop";
+//import ScrollToTop from "./utils/ScrollToTop";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/auth/register";
+import Login from './pages/auth/login';
+import Forgot_Password from './pages/auth/forgot_password';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
+    <>
+       <BrowserRouter>
+        {/*<ScrollToTop />*/}
       <Routes>
-        <Route path="/" element={<>you're here</>} />
+      <Route path="/" element={<Register/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/forgot-password" element={<Forgot_Password/>} />
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
