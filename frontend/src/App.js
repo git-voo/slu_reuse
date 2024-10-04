dea8a71ed563138633f1bffcc7a45b992dbf5804
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2024-10-03 20:56:30 -0500

    Update footer and fix code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,31 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProfileRoutes from './routes/profileRoutes';
 import "./App.css"
-// ////import Navigation from "./components/navigation";
 import Contactus from "./pages/contactus";
-
-// import ScrollToTop from "./utils/ScrollToTop";
- import LandingPage from "./pages/landingPage"
+import LandingPage from "./pages/landingPage"
 import Register from "./pages/auth/register";
 import Login from './pages/auth/login';
 import Forgot_Password from './pages/auth/forgot_password';
 import AboutUs from "./pages/aboutUs/AboutUs"
 import ProfilePage from './pages/profile/profilePage';
- 
 
+//import Navigation from "./components/navigation";
+// import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
       {/* <ScrollToTop /> */}
         <Routes >
-        <Route path = "/register" element = { < Register/ > }/> 
-        <Route path = "/login" element = { < Login/ > }/> 
-        <Route path = "/forgot-password" element = { < Forgot_Password/ > }/> 
-        <Route path = "/" element = { < LandingPage/ > }/> 
-        <Route path="/about" element={<AboutUs />} /> 
-      <Route path="/contact" element={<Contactus/>} />
-      <Route path="/profile" element={<ProfilePage/>} />
-       
+            <Route path = "/register" element = { < Register/ > }/> 
+            <Route path = "/login" element = { < Login/ > }/> 
+            <Route path = "/forgot-password" element = { < Forgot_Password/ > }/> 
+            <Route path = "/" element = { < LandingPage/ > }/> 
+            <Route path="/about" element={ < AboutUs/ > }/> 
+            <Route path="/contact" element={ < Contactus/ > }/>
+            <Route path="/profile" element={ < ProfilePage/ > }/>
         </Routes> 
-        </BrowserRouter> 
-        
+    </BrowserRouter> 
     )
 }
 
