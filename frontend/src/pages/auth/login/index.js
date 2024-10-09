d4d5a14832bff2fa2a4f3f83e6a62c4c9833d9c3
Shagun Sharma <shagun.sharma@slu.edu>
2024-10-08 23:44:15 -0500

    Update Login, Register jsx and css files to change UI

--- frontend/src/pages/auth/login/index.js
+++ frontend/src/pages/auth/login/index.js
@@ -2,8 +2,8 @@ import { useState }  from 'react'
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { FcGoogle } from 'react-icons/fc';
-import sluLogo from '../../../assets/slu-logo.png';
+//import { FcGoogle } from 'react-icons/fc';
+//import sluLogo from '../../../assets/slu-logo.png';
 import '../../../styles/auth/index.css';
 import '../../../styles/auth/login/index.css';
 import '../../../styles/button.css';
@@ -57,25 +57,25 @@ export default function Login() {
                 Sign in
               </Button>
 
-              <div className="divider">or</div>
+              {/*<div className="divider">or</div>
 
-              {/* SLU Email Sign Up Button */}
+              {// SLU Email Sign Up Button }
               <Button className="btn-auth slu-auth-btn default-btn-settings" onClick={handleSignin}>
                 <img src={sluLogo} alt="SLU Logo" className="auth-icon" />
                 Continue with SLU Email
               </Button>
 
-              {/* Google Sign Up Button */}
+              {// Google Sign Up Button }
               <Button className="btn-auth google-auth-btn default-btn-settings" onClick={handleSignin}>
                 <FcGoogle className="auth-icon" />
                 Continue with Google
               </Button>
 
-              {/* Facebook Sign Up Button */}
+              {// Facebook Sign Up Button}
               <Button className="btn-auth facebook-auth-btn default-btn-settings" onClick={handleSignin}>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" className="auth-icon" />
                 Continue with Facebook
-              </Button>
+              </Button>*/}
             </Form>
           </div>
           <p className="center-text">
--- frontend/src/pages/auth/register/index.js
+++ frontend/src/pages/auth/register/index.js
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { FcGoogle } from 'react-icons/fc';
-import sluLogo from '../../../assets/slu-logo.png';
+//import { FcGoogle } from 'react-icons/fc';
+//import sluLogo from '../../../assets/slu-logo.png';
+import { IoMdInformationCircleOutline } from 'react-icons/io';
 import '../../../styles/auth/index.css';
 import '../../../styles/auth/register/index.css';
 import '../../../styles/button.css';
 import { Link } from 'react-router-dom'; 
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 export default function Register() {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -57,29 +60,51 @@ export default function Register() {
                   <Form.Check type="checkbox" label="Remember me" />
                 </Form.Group>
 
+                <Form.Group className="mb-3" controlId="formBasicRegisterAs">
+                  <Form.Label style={{ fontWeight: 'bold' }}>Register as:</Form.Label>
+  
+                  <div className="register-as">
+                    <Form.Check type="checkbox" label="Donor?" />
+                    <OverlayTrigger placement="right" overlay={<Tooltip>As a donor, you can donate items to students in need.</Tooltip>}>
+                    <span className="info-icon">
+                    <IoMdInformationCircleOutline size={15} color="#1a2b49" />
+                    </span>
+                    </OverlayTrigger>
+                  </div>
+  
+                  <div className="register-as">
+                  <Form.Check type="checkbox" label="Student?" />
+                  <OverlayTrigger placement="right" overlay={<Tooltip>As a student, you can receive donated items from the community.</Tooltip>}>
+                  <span className="info-icon">
+                  <IoMdInformationCircleOutline size={15} color="#1a2b49" />
+                  </span>
+                 </OverlayTrigger>
+                  </div>
+                </Form.Group>
+
                   <Button className="btn-join btn-blue default-btn-settings" variant="primary" type="submit" onClick={handleSignup}>
                   Sign Up
                 </Button>
 
-                <div className="divider">or</div>
+                {/*<div className="divider">or</div>
 
-                {/* SLU Email Sign Up Button */}
+                {// SLU Email Sign Up Button }
                 <Button className="btn-auth slu-auth-btn default-btn-settings" onClick={handleSignup}>
                   <img src={sluLogo} alt="SLU Logo" className="auth-icon" />
                   Continue with SLU Email
                 </Button>
 
-                {/* Google Sign Up Button */}
+                {// Google Sign Up Button }
                 <Button className="btn-auth google-auth-btn default-btn-settings" onClick={handleSignup}>
                   <FcGoogle className="auth-icon" />
                   Continue with Google
                 </Button>
 
-                {/* Facebook Sign Up Button */}
+                {// Facebook Sign Up Button }
                 <Button className="btn-auth facebook-auth-btn default-btn-settings" onClick={handleSignup}>
                   <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" className="auth-icon" />
                   Continue with Facebook
-                </Button>
+                </Button>*/}
 
                 <p className="center-text">
                   Already a member? <Link to="/login">Sign in</Link>
