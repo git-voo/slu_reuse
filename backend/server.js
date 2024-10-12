b91e8c553b40106f52a77305703b3aa07eee4e19
Shagun Sharma <shagun.sharma@slu.edu>
2024-10-11 22:48:17 -0500

    add auth route and handle error in the  backend server file

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 import express from "express"
 import bodyParser from "body-parser"
 import path from "path"
-import cors from "cors" 
+import cors from "cors"
 import connectDB from "./DB/connectDB.js"
 import { configDotenv } from "dotenv"
 import { fileURLToPath } from 'url'
-
 import itemRoutes from "./routes/itemRoutes.js"
+import authRoutes from './routes/authRoutes.js';
 
 const router = express.Router()
+
+//express app
 const app = express()
-const __filename = fileURLToPath(import.meta.url)
+
+const __filename = fileURLToPath(
+    import.meta.url)
+
 const __dirname = path.dirname(__filename)
 
 configDotenv()
@@ -22,15 +27,23 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(cors())
 
 const documentation = router.get("/", (req, res) => {
-  res.send("API Documentation page")
+    res.send("API Documentation page")
 })
 
 app.use("/", documentation)
 app.use("/api", itemRoutes)
- 
+app.use('/api/auth', authRoutes);
+
 connectDB()
 
-app.listen(PORT, () => {
-  console.log(`Running on   http://localhost:${PORT}`)
+//lisen for requests
+const server = app.listen(PORT, () => {
+    console.log(`Running on   http://localhost:${PORT}`)
 });
 
+server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+        console.log(`Port ${PORT} is already in use`);
+        process.exit(1);
+    }
+});
