50694507d941595ee4182fd0e61e1aaf75f0e76f
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2024-10-13 21:37:48 -0500

    Update sidebar

--- frontend/src/components/profile/sidebar/index.jsx
+++ frontend/src/components/profile/sidebar/index.jsx
@@ -1,23 +1,29 @@
 import './sidebar.css';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <ul>
                 <li>
-                    <a href="/my-listing">My Listing</a>
+                    <Link to="/my-listings">My Listings</Link>
                 </li>
                 <li>
-                    <a href="/list-item">List an Item</a>
+                    <Link to="/list-item">List an Item</Link>
                 </li>
                 <li>
-                    <a href="/conversations">Conversations</a>
+                    <Link to="/conversations">Conversations</Link>
                 </li>
                 <li>
-                    <a href="/verify-email">Verify Email</a>
+                    <Link to="/verify-email">Verify Email</Link>
                 </li>
                 <li>
-                    <a href="/update-profile">Update Profile</a>
+                    <Link to="/update-profile">Update Profile</Link>
+                </li>
+            </ul>
+            <ul className="bottom">
+                <li className="delete-account">
+                    <Link to="/delete-account">Delete My Account</Link>
                 </li>
             </ul>
         </div>
