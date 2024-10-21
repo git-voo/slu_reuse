2b1c5ce2d2a63601b96b5e0ee142f13517b98ef9
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2024-10-21 01:42:10 -0500

    Fix AxiosConfig incorrect token

--- frontend/src/services/AxiosConfig.js
+++ frontend/src/services/AxiosConfig.js
@@ -2,28 +2,24 @@ import axios from "axios";
 import { ErrorHandler } from "./ErrorHandler";
 
 export default function AxiosConfig() {
-  const token = localStorage.getItem("stucademy-tks");
+    const instanceConfig = axios.create({
+        baseURL: process.env.REACT_APP_BASE_URL,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    });
 
-  const instanceConfig = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    headers: {
-      common: {
-        Authorization: token ? `Bearer ${token}` : null,
-      },
-    },
-  });
+    instanceConfig.interceptors.response.use(
+        (response) => handleSuccess(response),
+        (error) => {
+            ErrorHandler(error);
+            return Promise.reject(error);  // Propagate the error
+        }
+    );
 
-  instanceConfig.interceptors.response.use(
-    (response) => handleSuccess(response),
-    (error) => {
-      ErrorHandler(error);
-      return Promise.reject(error); // Ensure errors are propagated
+    function handleSuccess(response) {
+        return response;  // Return successful response
     }
-  );
 
-  function handleSuccess(response) {
-    return response;
-  }
-
-  return instanceConfig;
+    return instanceConfig;
 }
