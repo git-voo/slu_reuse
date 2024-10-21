630cb9e682e56d3df42e7f33dae3b344af2665a0
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2024-10-21 00:36:25 -0500

    Update AxiosConfig.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { check, body } from 'express-validator';
 import { register, verifyEmail, login, forgotPassword, verifyResetCode, resetPassword, getProfile, updateProfile } from '../controllers/authController.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 
 const router = express.Router();
