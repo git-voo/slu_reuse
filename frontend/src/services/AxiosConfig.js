89d331403b2a6702477c9721f8947f48dfce7a26
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2024-10-21 01:32:54 -0500

    Update profile page

--- frontend/src/services/AxiosConfig.js
+++ frontend/src/services/AxiosConfig.js
@@ -2,28 +2,28 @@ import axios from "axios";
 import { ErrorHandler } from "./ErrorHandler";
 
 export default function AxiosConfig() {
-    const token = localStorage.getItem("stucademy-tks");
+  const token = localStorage.getItem("stucademy-tks");
 
-    const instanceConfig = axios.create({
-        baseURL: process.env.REACT_APP_BASE_URL,
-        headers: {
-            common: {
-                Authorization: token ? `Bearer ${token}` : null,
-            },
-        },
-    });
+  const instanceConfig = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+    headers: {
+      common: {
+        Authorization: token ? `Bearer ${token}` : null,
+      },
+    },
+  });
 
-    instanceConfig.interceptors.response.use(
-        (response) => handleSuccess(response),
-        (error) => {
-            ErrorHandler(error);
-            return Promise.reject(error); // Ensure errors are propagated
-        }
-    );
-
-    function handleSuccess(response) {
-        return response;
+  instanceConfig.interceptors.response.use(
+    (response) => handleSuccess(response),
+    (error) => {
+      ErrorHandler(error);
+      return Promise.reject(error); // Ensure errors are propagated
     }
+  );
 
-    return instanceConfig;
+  function handleSuccess(response) {
+    return response;
+  }
+
+  return instanceConfig;
 }
