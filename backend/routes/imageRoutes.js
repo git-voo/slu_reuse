18256bbb699e98414e144095624934f140b9ad15
vishalreddy9866 <vishalreddy9866@gmail.com>
2024-11-13 21:24:34 -0600

    feat: implement Google Drive upload functionality

--- backend/routes/imageRoutes.js
+++ backend/routes/imageRoutes.js
@@ -1,15 +1,29 @@
 import express from 'express';
-import upload from '../utils/cloudinaryConfig/multerConfig.js';
-
-
-
+import { drive } from '../utils/googleDriveConfig.js';
+import multer from 'multer';
 
 const router = express.Router();
+const upload = multer();
 
-router.post('/upload', upload.single('image'), (req, res) => {
+router.post('/upload', upload.single('image'), async (req, res) => {
   try {
-    // Send back the Cloudinary URL of the uploaded image
-    res.status(200).json({ url: req.file.path });
+    const fileMetadata = {
+      name: req.file.originalname,
+      parents: ['1p1w12_CRC1stpsQaw9eMX-KDSz1e93se'], //  actual folder ID in Google Drive
+    };
+
+    const media = {
+      mimeType: req.file.mimetype,
+      body: Buffer.from(req.file.buffer),
+    };
+
+    const response = await drive.files.create({
+      requestBody: fileMetadata,
+      media: media,
+      fields: 'id, webViewLink',
+    });
+
+    res.status(200).json({ url: response.data.webViewLink });
   } catch (error) {
     res.status(500).json({ error: 'Failed to upload image' });
   }
