26a6edbf748fe8f7e6b2c6aa56bb3dd5220c116d
Onoja <onoja.jsdev@gmail.com>
2024-11-28 12:11:24 -0600

    update caption service top get service url from env

--- backend/routes/analyzeImageRoutes.js
+++ backend/routes/analyzeImageRoutes.js
@@ -1,36 +1,37 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+import express from "express"
+import axios from "axios"
+import dotenv from "dotenv"
+dotenv.config()
 
-const router = express.Router();
+const router = express.Router()
 
 export async function getCaptionFromBLIP(imageUrl) {
+    const PYTHON_SERVICE_URL = process.env.PYTHON_SERVICE_URL
     try {
-        const response = await axios.post('http://127.0.0.1:4301/caption', { imageUrl });
-        return response.data.caption;
+        const response = await axios.post(`${PYTHON_SERVICE_URL}/caption`, { imageUrl })
+        return response.data.caption
     } catch (error) {
-        console.error("Error fetching caption from BLIP API:", error);
-        throw new Error("Failed to get caption");
+        console.error("Error fetching caption from BLIP API:", error)
+        throw new Error("Failed to get caption")
     }
 }
 
 // Route to analyze image
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
     console.log(req.body)
-    const { imageUrl } = req.body;
+    const { imageUrl } = req.body
 
     if (!imageUrl) {
-        return res.status(400).json({ error: "Image URL is required" });
+        return res.status(400).json({ error: "Image URL is required" })
     }
     try {
         // Call the Python microservice to get the caption
-        const caption = await getCaptionFromBLIP(imageUrl);
-        res.json({ description: caption });
+        const caption = await getCaptionFromBLIP(imageUrl)
+        res.json({ description: caption })
     } catch (error) {
-        console.error("Error analyzing image:", error);
-        res.status(500).json({ error: "Error analyzing image" });
+        console.error("Error analyzing image:", error)
+        res.status(500).json({ error: "Error analyzing image" })
     }
-});
+})
 
-export default router;
+export default router
