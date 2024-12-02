e26f3265b25329c8252167759ebe819b60ca0e7d
Shagun Sharma <shagun.sharma@slu.edu>
2024-12-02 17:39:57 -0600

    fix login user navigation issue

--- frontend/src/services/AxiosInstance.js
+++ frontend/src/services/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { Navigate } from "react-router-dom"
 
-const token = localStorage.getItem("token")
+/*const token = localStorage.getItem("token")
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
@@ -11,16 +11,40 @@ const axiosInstance = axios.create({
             "Authorization": `Bearer ${token}`
         },
     },
-})
+})*/
+
+const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+
+// Adding a request interceptor to add the token dynamically for each request
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 
 axiosInstance.interceptors.response.use(
     (response) => handleSuccess(response),
     (error) => handleError(error)
 )
+
 function handleSuccess(response) {
     return response
 }
+
 function handleError(error) {
     if (error.response && error.response.code === 401) {
         Navigate({ to: "/login", replace: true })
